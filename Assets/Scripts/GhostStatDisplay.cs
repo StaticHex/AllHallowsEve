3ba364e116e2d8f9d6a255b2d13b0340be681d42
Matthewit Dechatech <mdechatech@gmail.com>
2015-10-17 22:47:44 -0500

    Fix ui scaling, add ghost ui

---
+++ Assets/Scripts/GhostStatDisplay.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GhostStatDisplay : MonoBehaviour {
+
+    [SerializeField]
+    public Hero Player;
+
+    [SerializeField]
+    public Text Name;
+    [SerializeField]
+    public Image HealthBar;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Name.text = Player.name;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(!Player) Destroy(gameObject);
+
+	    var healthScale = Player.health/Player.maxHealth;
+        HealthBar.transform.localScale = new Vector3(HealthBar.transform.localScale.x,
+            HealthBar.transform.localScale.y,
+            healthScale);
+    }
+
+    public void LateUpdate()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            0.0f);
+        transform.position = Player.transform.position;
+    }
+}
--- Assets/Scripts/PlayersDisplay.cs
+++ Assets/Scripts/PlayersDisplay.cs
@@ -4,18 +4,25 @@ using System.Collections;
 public class PlayersDisplay : MonoBehaviour
 {
     [SerializeField] private HeroStatDisplay _baseHeroStatDisplay;
+    [SerializeField] private GhostStatDisplay _baseGhostStatDisplay;
 
 	// Use this for initialization
 	void Start () {
 	    foreach (var heroObject in FindObjectsOfType<Hero>())
 	    {
-	        var hero = (Hero) heroObject;
+	        var hero = heroObject;
 	        if (hero.GetComponent<BeamWeapon>() != null)
 	        {
                 var heroStatDisplay = Instantiate(_baseHeroStatDisplay);
                 heroStatDisplay.Player = hero;
                 heroStatDisplay.transform.SetParent(null);
 	            heroStatDisplay.transform.position = heroObject.transform.position;
+	        } else if (hero.GetComponent<GhostInjury>() != null)
+	        {
+	            var ghostStatDisplay = Instantiate(_baseGhostStatDisplay);
+	            ghostStatDisplay.Player = hero;
+                ghostStatDisplay.transform.SetParent(null);
+                ghostStatDisplay.transform.position = heroObject.transform.position;
 	        }
 	    }
 	}
